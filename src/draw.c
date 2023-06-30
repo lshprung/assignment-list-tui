3fc10846ca2d1b42b596fbf3d719abc1d6608c94
Louie S <louie@example.com>
2023-06-30 15:45:16 -0700

    Create border

--- src/draw.c
+++ src/draw.c
@@ -5,6 +5,7 @@
 
 void tui_init() {
 	int input; // capture user input
+	WINDOW *w_main;
 	
 	// set an interrupt to end cleanly
 	signal(SIGINT, tui_end);
@@ -13,6 +14,13 @@ void tui_init() {
 	initscr();
 	keypad(stdscr, TRUE); // enable arrow keys and other function keys
 	cbreak();             // set for interactive curses applications
+	curs_set(0);             // hide cursor
+
+	// set main window
+	w_main = newwin(0, 0, 0, 0);
+	box(w_main, 0, 0);
+	refresh();
+	wrefresh(w_main);
 
 	input = getch();
 	tui_end();
